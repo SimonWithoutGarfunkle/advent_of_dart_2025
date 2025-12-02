317835895268e2f999db6aba6c83358a42cb6d29
SimonWithoutGarfunkle <123770440+SimonWithoutGarfunkle@users.noreply.github.com>
2025-12-02 22:19:05 +0100

    day 2 question 1

--- lib/days/day2.dart
+++ lib/days/day2.dart
@@ -1,9 +1,40 @@
 import '../services/input_service.dart';
 
 Future<String> solvePuzzle1() async {
-  return 'Ca arrive !';
+  final lines = await const InputService().readInputLines(2);
+  final ranges = lines.first.split(',').map(_parseRange).toList(growable: false);
+  int result = 0;
+
+  for (final r in ranges) {
+      for (int i = r.min; i <= r.max; i++) {
+        if (i.toString().length.isOdd) continue;
+        final txt = i.toString();
+        final mid = txt.length ~/ 2;
+        if (txt.substring(0, mid) == txt.substring(mid)) {
+          result += int.parse(txt);
+        }
+      }
+  }
+  return 'Value: $result';
 }
 
 Future<String> solvePuzzle2() async {
   return 'Ca arrive !';
+}
+
+class Range{
+  final int min;
+  final int max;
+
+  const Range({
+    required this.min,
+    required this.max,
+  });
+}
+
+Range _parseRange(String input) {
+  return Range(
+      min: int.parse(input.split('-')[0]),
+      max: int.parse(input.split('-')[1]),
+  );
 }
