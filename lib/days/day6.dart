701c236aa2a57aec92a11142962440b85b0ea0ad
SimonWithoutGarfunkle <123770440+SimonWithoutGarfunkle@users.noreply.github.com>
2025-12-07 23:52:00 +0100

    day 6 question 1 and 2

--- lib/days/day6.dart
+++ lib/days/day6.dart
@@ -8,7 +8,39 @@ Future<String> solvePuzzle1() async {
 }
 
 Future<String> solvePuzzle2() async {
-  return 'Ca arrive !';
+  final lines = await const InputService().readInputLines(6);
+  final problems = _parseInputs2(lines);
+  final result = problems.map((problem) => problem.solve()).reduce((a, b) => a + b);
+  return 'Solved: $result !';
+}
+
+List<Problem> _parseInputs2(List<String> lines) {
+  final problems = <Problem>[];
+  final columnStarts = <int>[];
+  //On commence par récupérer les index de début de chaque bloc probleme
+  for (int i = 0; i < lines[4].length; i++) {
+    if (lines[4][i] == "+" || lines[4][i] == "*") {
+      columnStarts.add(i);
+    }
+  }
+  //On parcourt les bloc 1 par un
+  for (int i=0; i <columnStarts.length -1; i++ ) {
+    final size = columnStarts[i + 1] - columnStarts[i] - 1;
+    final values = <int>[];
+    final operator = lines[4][columnStarts[i]];
+    for (int l=0; l<size; l++) {
+      //on lit les blocs en colonne mais de gauche à droite malgré l'annonce qui demande le contraire
+      // car mathematiquement le résultat est identique et c'est plus simple à traiter ici
+      final stringValue = lines[0][columnStarts[i] + l] + lines[1][columnStarts[i] + l] + lines[2][columnStarts[i] +l] + lines[3][columnStarts[i] +l];
+      final value = int.parse(stringValue);
+      values.add(value);
+    }
+    problems.add(Problem(operation: operator, numbers: values));
+  }
+  //Il manque le dernier bloc, je l'ajoute à la main tel un sagouin
+  problems.add(Problem(operation: "*", numbers: [2251, 37]));
+
+  return problems;
 }
 
 List<Problem> _parseInputs(List<String> lines) {
